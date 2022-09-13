720bb0480a9edd260c978f70551a341419ed47c7
Sadaf Saleem <sadafsaleem5815@gmail.com>
2022-09-13 21:01:21 +0500

    Update code.py

--- code.py
+++ code.py
@@ -41,3 +41,25 @@ netflix_df_movies_only = netflix_df[netflix_df['type'] == 'Movie']
 # Select only the columns of interest
 netflix_movies_col_subset = netflix_df_movies_only[['title', 'country', 'genre', 'release_year', 'duration']]
 
+# Print the first five rows of the new DataFrame
+netflix_movies_col_subset[0:5]
+
+# Create a figure and increase the figure size
+fig = plt.figure(figsize=(12,8))
+
+# Create a scatter plot of duration versus year
+plt.scatter(netflix_movies_col_subset["release_year"], netflix_movies_col_subset["duration"])
+
+# Create a title
+plt.title("Movie Duration by Year of Release")
+
+# Show the plot
+plt.show()
+
+# Filter for durations shorter than 60 minutes
+short_movies = netflix_movies_col_subset[netflix_movies_col_subset['duration'] <60]
+
+# Print the first 20 rows of short_movies
+print(short_movies[0:20])
+
+
