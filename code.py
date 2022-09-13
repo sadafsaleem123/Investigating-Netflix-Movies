56eb96c2953da1dfd30c72c6668de6b040b0ae23
Sadaf Saleem <sadafsaleem5815@gmail.com>
2022-09-13 21:00:58 +0500

    Update code.py

--- code.py
+++ code.py
@@ -7,3 +7,37 @@ movie_dict = {'years': years, 'durations': durations}
 # Print the dictionary
 movie_dict
 
+# Import pandas under its usual alias
+import pandas as pd
+
+# Create a DataFrame from the dictionary
+durations_df = pd.DataFrame(movie_dict)
+
+# Print the DataFrame
+print(durations_df)
+
+# Import matplotlib.pyplot under its usual alias and create a figure
+import matplotlib.pyplot as plt
+fig = plt.figure()
+
+# Draw a line plot of release_years and durations
+plt.plot(durations_df['years'], durations_df['durations'])
+
+# Create a title
+plt.title("Netflix Movie Durations 2011-2020")
+
+# Show the plot
+plt.show()
+
+# Read in the CSV as a DataFrame
+netflix_df = pd.read_csv("datasets/netflix_data.csv")
+
+# Print the first five rows of the DataFrame
+print(netflix_df[0:5])
+
+# Subset the DataFrame for type "Movie"
+netflix_df_movies_only = netflix_df[netflix_df['type'] == 'Movie']
+
+# Select only the columns of interest
+netflix_movies_col_subset = netflix_df_movies_only[['title', 'country', 'genre', 'release_year', 'duration']]
+
